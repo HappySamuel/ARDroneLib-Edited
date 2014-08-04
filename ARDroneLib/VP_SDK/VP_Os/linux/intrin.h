57bb9435ed6f30bd896ec6c08c857891f1ab494b
infdsc02 <infdsc02@gmail.com>
2014-08-05 05:16:52 +1000

    KK

--- ARDroneLib/VP_SDK/VP_Os/linux/intrin.h
+++ ARDroneLib/VP_SDK/VP_Os/linux/intrin.h
@@ -7,6 +7,8 @@
 // #undef always_inline
 #if defined(__GNUC__)
 
+#if TARGET_CPU_X86 == 1
+
 static INLINE uint32_t _byteswap_ulong(uint32_t value)
 {
   uint32_t ret;
@@ -45,9 +47,55 @@ static INLINE uint32_t _byteswap_ulong(uint32_t value)
   return value;
 }
 
+#endif // TARGET_CPU_X86
+
+#if TARGET_CPU_ARM == 1
+
+#ifdef TARGET_OS_IPHONE
+
+static INLINE uint32_t _byteswap_ulong(uint32_t value)
+{
+  uint32_t ret;
+
+  __asm __volatile (
+                    " rev %0, %1\n"
+                    : "=r" (ret)
+                    : "r" (value)
+                    );
+
+  return ret;
+}
+
+#define usat16( value, imm )      __asm __volatile( "usat16 %0, %1, %0" : "=r" (value) : "0" (value), "I" (imm) )
+#define usat( value, imm, shift ) __asm __volatile( "usat %0, %2, %0, asr %3" :"=r" (value) :"0" (value), "I" (imm), "I" (shift) )
+
+#define uxtb( ret, value, imm )   __asm __volatile ( "uxtb    %0, %1, ror %2" : "=r" (ret) : "r" (value), "I" (imm) )
+#define uxtb16( ret, value, imm ) __asm __volatile ( "uxtb16  %0, %1, ror %2" : "=r" (ret) : "r" (value), "I" (imm) )
+#define uxth( ret, value, imm )   __asm __volatile ( "uxth    %0, %1, ror %2" : "=r" (ret) : "r" (value), "I" (imm) )
+
+#else
+
+static INLINE uint32_t _byteswap_ulong(uint32_t value)
+{
+  int32_t tmp;
+
+  __asm __volatile(
+    "eor	%1, %2, %2, ror #16\n"
+    "bic	%1, %1, #0x00ff0000\n"
+    "mov	%0, %2, ror #8\n"
+    "eor	%0, %0, %1, lsr #8"
+    : "=r" (value), "=r" (tmp)
+    : "r" (value)
+  );
+
+  return value;
+}
+
+#endif // TARGET_OS_IPHONE
 
 #define clz   __builtin_clz
 
+#endif // TARGET_CPU_ARM
 
 #define bswap _byteswap_ulong
 
