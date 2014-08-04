7b0cc5c5a457b0ed3ce84731a30ad27c61cdd3b4
infdsc02 <infdsc02@gmail.com>
2014-08-05 03:15:29 +1000

    Editado para que funcione con ARM

--- ARDroneLib/VP_SDK/VP_Os/linux/intrin.h
+++ ARDroneLib/VP_SDK/VP_Os/linux/intrin.h
@@ -7,43 +7,6 @@
 // #undef always_inline
 #if defined(__GNUC__)
 
-#if TARGET_CPU_X86 == 1
-
-static INLINE uint32_t _BitScanReverse(uint32_t* index, uint32_t mask)
-{
-  __asm__("bsrl %[mask], %[index]" : [index] "=r" (*index) : [mask] "mr" (mask));
-
-  return mask ? 1 : 0;
-}
-
-
-static INLINE uint32_t _byteswap_ulong(uint32_t value)
-{
-  __asm("bswap %0":
-      "=r" (value):
-      "0" (value));
-
-  return value;
-}
-
-static inline uint32_t clz(uint32_t code)
-{
-  uint32_t index = 0;
-  if( code )
-  {
-    _BitScanReverse(&index, code);
-    index ^= 31;
-  }
-
-  return index;
-}
-
-#endif // TARGET_CPU_X86
-
-#if TARGET_CPU_ARM == 1
-
-#ifdef TARGET_OS_IPHONE
-
 static INLINE uint32_t _byteswap_ulong(uint32_t value)
 {
   uint32_t ret;
@@ -82,11 +45,9 @@ static INLINE uint32_t _byteswap_ulong(uint32_t value)
   return value;
 }
 
-#endif // TARGET_OS_IPHONE
 
 #define clz   __builtin_clz
 
-#endif // TARGET_CPU_ARM
 
 #define bswap _byteswap_ulong
 
